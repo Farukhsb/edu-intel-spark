6cd0bc803ea81b571c4a83ddefdec252236db3a3
farukhsb <farukhsb@gmail.com>
2026-04-26 19:58:42 +0100

    Add privacy-safe Sentry error monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initSentry } from "@/lib/sentry";
 import { initPostHog } from "./lib/posthog";
+
+initSentry();
 
 const scheduleAnalyticsInit = () => {
   if ("requestIdleCallback" in window) {
